f5a65fad7586f9f41449f28c691a5c58788e0296
Your Name <you@example.com>
2025-10-07 14:44:22 +0530

    add postgresql

--- tweethq/settings.py
+++ tweethq/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = 'tweethq.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ.get('PG_NAME'),
+    'USER': os.environ.get('PG_USER'),
+    'PASSWORD': os.environ.get('PG_PASSWORD'),
+    'HOST': os.environ.get('PG_HOST'),
+    'PORT': os.environ.get('PG_PORT', '5432'),
+  }
 }
 
 
